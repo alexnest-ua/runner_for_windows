d0d93af2b49a954e1ff94441a6a5804960c855c2
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-04-13 14:10:45 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ cd ~
 rm -r mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd ~/mhddos_proxy
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 cd ~/runner_for_windows
