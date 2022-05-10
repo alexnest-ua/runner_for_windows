56bbc16c68bf0eb5eca4585cdd304399dc30db10
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-10 19:20:50 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-cd ~
-rm -rf mhddos_proxy
-git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
-cd ~/mhddos_proxy
-python -m pip install -r requirements.txt
-cd ~/runner_for_windows
