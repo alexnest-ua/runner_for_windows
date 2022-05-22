d784b1fdb53da2d4030c8cf7a3e550e0ddb7f8f6
alexnest-ua <74729549+alexnest-ua@users.noreply.github.com>
2022-05-22 19:19:12 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -262,7 +262,7 @@ do
             		echo "python runner.py $cmd_line --rpc $rpc -t $threads $vpn $debug"
             
             		cd ~/mhddos_proxy
-            		python runner.py $cmd_line -t $threads $vpn --lang en $debug&
+            		python runner.py $cmd_line -t $threads $vpn $debug&
 	    		sleep 20s
 			echo -e "\n[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - \033[42mAttack started successfully\033[0m\n"
 		done
